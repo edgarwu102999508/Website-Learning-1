ec13b6563ce7e8ae2d193006d477d34a237b1fe3
edgarwu102999508 <102999508@student.swin.edu.au>
2020-10-14 10:17:29 +1100

    Project setup

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <h3>Navbar</h3>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/components/pages/home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h3>Home Page Here</h3>
+    </div>
+  );
+}
+export default Home;
